e05cf7b806146e5348cab9cfc96769fc4743a3e0
Gibo2706 <bogdan.markovic2706@gmail.com>
2025-11-03 23:48:43 +0100

    initialize SmartTaskTracker project with Gradle setup and basic Spring Boot application structure

---
+++ backend/src/test/java/com/example/smarttasktracker/SmartTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarttasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
